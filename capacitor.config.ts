d074aac550122cd44a1f9a8a0545be6bf42e3f2f
Nicholas Irving <developer@darkedges.com>
2024-12-20 19:06:45 +1100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-test-resume',
+  webDir: 'dist'
+};
+
+export default config;
